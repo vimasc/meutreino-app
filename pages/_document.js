afd8d17f604423160b938f381f6924540a957c17
vimasc <vimasc@gmail.com>
2026-05-10 21:05:04 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,9 @@ export default function Document() {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="MeuTreinoIA" />
+        <meta name="apple-mobile-web-app-title" content="VMIACoachTreino" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
+        <title>VMIACoachTreino</title>
       </Head>
       <body>
         <Main />
